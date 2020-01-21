f23a94c4cfbe20f4843dfd848ea5f45734c0123c
川﨑 大河 <kawasaki.taiga@raccoon.ne.jp>
2020-01-21 19:27:16 +0900

    課題1追加

---
+++ async/async1.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+/*
+# 課題1
+goroutineを使ってみよう
+プログラムはF5を押せばデバックできるよ！
+
+# 期待する結果
+標準出力に
+hello
+kawasaki
+と表示される
+*/
+func main() {
+	sayHello()
+	fmt.Println("kawasaki")
+	time.Sleep(2 * time.Second)
+}
+
+func sayHello() {
+	fmt.Println("hello")
+}
