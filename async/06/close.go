5c59b400bd0e80be334ffdd8bec564eaa8050492
川﨑 大河 <kawasaki.taiga@raccoon.ne.jp>
2020-01-22 14:07:31 +0900

    closeのTry追加

---
+++ async/06/close.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	messages := make(chan string)
+	go func() {
+		for i := 0; i < 5; i++ {
+			messages <- "処理中"
+		}
+	}()
+	// 無限ループ
+	for {
+		msg, ok := <-messages
+		if ok {
+			fmt.Println(msg)
+			continue
+		}
+		fmt.Println("処理終了")
+		break
+	}
+}
